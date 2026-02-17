457c3fe12408d15d3fb0ce7534c9588bba9bf4b2
Samandeep Sidhu <samansidhu0125@gmail.com>
2026-02-17 22:55:11 +0530

    Starters-0 changes done

--- backend/python/django_app/urls.py
+++ backend/python/django_app/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from django.http import HttpResponse
 
 def hello_world(request):
-    return HttpResponse("Hello, world! This is our interneers-lab Django server.")
+    return HttpResponse("Hello, world! This is our interneers-lab Django server" 
+                + ", and this function has been modified for week-1 starters-0 change.")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
