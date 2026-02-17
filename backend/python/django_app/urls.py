47b24492e98383ebfd0e9290ecf8d264abdde166
Samandeep Sidhu <samansidhu0125@gmail.com>
2026-02-17 23:10:02 +0530

    Starters-1 changes done

--- backend/python/django_app/urls.py
+++ backend/python/django_app/urls.py
@@ -1,12 +1,25 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 def hello_world(request):
     return HttpResponse("Hello, world! This is our interneers-lab Django server" 
                 + ", and this function has been modified for week-1 starters-0 change.")
 
+def hello_name(request):
+    """
+    This function recieves a request object and returns 'Hello, {name}!' in JSON format.
+    It extracts the name from query parameter called 'name' in request object.
+    """
+    name = request.GET.get('name')
+    return JsonResponse({
+        'message': f'Hello, {name}!'
+    })
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', hello_world),
+    path('hello_world/', hello_world),
+    path('hello/', hello_name),
+    # Example usage: /hello/?name=Bob
+    # returns {"message": "Hello, Bob!"}
 ]
